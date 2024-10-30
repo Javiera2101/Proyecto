72e796665761e982956b276aada71634ed670198
Javiera2101 <158289623+Javiera2101@users.noreply.github.com>
2024-10-30 09:51:11 -0300

    Delete Proyecto directory

--- Proyecto/proyecto/demo/src/main/java/com/example/demo/repository/PoloRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.demo.model.Polo;
-
-public interface PoloRepository extends JpaRepository <Polo, Integer>{
-}
