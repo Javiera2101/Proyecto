6f95b43c0b4cb3a7afb21f67dcb164464d187e11
Javiera2101 <158289623+Javiera2101@users.noreply.github.com>
2024-10-30 09:03:43 -0300

    Delete Proyecto-7cb3444ebb0966783c5438d8096c5fc8550a28f5/proyecto/demo directory

--- Proyecto-7cb3444ebb0966783c5438d8096c5fc8550a28f5/proyecto/demo/src/main/java/com/example/demo/repository/AcademicoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.demo.model.Academico;
-
-public interface AcademicoRepository extends JpaRepository <Academico, Integer>{
-    
-}
