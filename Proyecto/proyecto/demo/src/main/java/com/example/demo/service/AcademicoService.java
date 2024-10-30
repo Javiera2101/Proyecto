c73378d5b85ca02cb677d09795d7c05acecc4337
Javiera2101 <158289623+Javiera2101@users.noreply.github.com>
2024-10-30 09:58:47 -0300

    Add files via upload

---
+++ Proyecto/proyecto/demo/src/main/java/com/example/demo/service/AcademicoService.java
@@ -0,0 +1,16 @@
+package com.example.demo.service;
+
+import java.util.List;
+
+import com.example.demo.model.Academico;
+
+public interface AcademicoService {
+    
+    public List<Academico> buscarTodosLosAcademicos();
+    
+    public Academico buscarAcademicoPorId(int id);
+    
+    public void Guardar(Academico academico);
+    
+    public void BorrarAcademicoPorId(int id);
+}
